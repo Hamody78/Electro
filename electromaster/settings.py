d5d9329c734cbcee21f32dac29d50a97f1cb6d9e
Mohamed Ramadan Saad <mohamedaayk@gmail.com>
2024-07-25 17:10:46 +0300

    Second Commit

--- electromaster/settings.py
+++ electromaster/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m&yg@-x6iaai^d+490k9ic)wn72)n@as8vu_ixm8dh(f746*qe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ["127.0.0.1:8000", "127.0.0.1", "localhost", "localhost:8000"]
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://hamody78.github.io/Electro/"]
 
 
 # Application definition
